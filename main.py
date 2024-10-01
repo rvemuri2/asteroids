d2b9cf2a518254f7317107ab6e1e0052f03f15aa
rvemuri2 <rohitvemuri@yahoo.com>
2024-09-30 20:12:22 -0700

    Added score and countdown timer to improve game mechanics

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    counter, text = 60, '60'.rjust(1)
+    score_counter = 0
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+    font = pygame.font.SysFont('Consolas', 30)
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -29,10 +33,23 @@ def main():
     while True:
         for event in pygame.event.get():
 
+            if(event.type == pygame.USEREVENT):
+                if counter > 0:
+                    text = "Time: " + str(counter).rjust(1)
+                else:
+                    print("Time's Up!")
+                    print("Game Score:", score_counter)
+                    sys.exit()
+                counter -= 1
+
             if event.type == pygame.QUIT:
                 return
             
         pygame.Surface.fill(screen, "black")
+        label = font.render(text, 1, (255, 0, 0))
+        score_label = font.render(f'Score: {score_counter}', True, (0, 255, 0))
+        screen.blit(score_label, (1100, 50))
+        screen.blit(label, (40, 50))
 
         for sprite in drawable:
             sprite.draw(screen)
@@ -47,9 +64,11 @@ def main():
         for asteroid in asteroids:
             if asteroid.collisions(player):
                 print("Game Over!")
+                print("Game Score:", score_counter)
                 sys.exit()
             for bullet in shots:
                 if(asteroid.collisions(bullet)):
+                    score_counter += 1
                     asteroid.split()
                     bullet.kill()
 
